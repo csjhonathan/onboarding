3b3360b2a8133745962a9d8acf055d4634918439
csjhonathan <jhonathancarv.s@gmail.com>
2023-06-22 10:16:39 -0300

    fix: query limit

--- src/repositories/passengers.repository.js
+++ src/repositories/passengers.repository.js
@@ -1,7 +1,7 @@
 import connect from '../database/database.js';
 import passengerQueryBuilder from '../utils/passengersQueryBuilder.js';
 
-async function listPassengersTravels(page = 0, name = '', limit = 101) {
+async function listPassengersTravels(page = 0, name = '', limit = 100) {
   const query = passengerQueryBuilder(page, name, limit)
   const { rows } = await connect.query(query);
 
